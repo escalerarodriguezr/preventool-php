73b9016a496a55b3f2c3aa58aecfa906b1d4898e
Rafael Escalera <escalerarodriguezr@gmail.com>
2022-11-07 23:37:19 +0100

    config-monolog

--- preventool/web/Controller/Demo/GetDemoController.php
+++ preventool/web/Controller/Demo/GetDemoController.php
@@ -3,11 +3,25 @@ declare(strict_types=1);
 
 namespace App\Controller\Demo;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
 class GetDemoController
 {
-    public function __invoke()
+
+
+    public function __construct(
+        private LoggerInterface $logger
+    )
     {
-        dd("llega");
+    }
+
+    public function __invoke():Response
+    {
+        $this->logger->error("hola...");
+
+        return new JsonResponse(null,200);
     }
 
 
