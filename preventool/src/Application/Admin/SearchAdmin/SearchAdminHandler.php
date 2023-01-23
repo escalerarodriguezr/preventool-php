24a0bc3da4c459400925ca30375b4dd1d972adf1
Rafael Escalera <escalerarodriguezr@gmail.com>
2023-01-23 13:37:39 +0100

    feature/5/setup-use-case-permissions

--- preventool/src/Application/Admin/SearchAdmin/SearchAdminHandler.php
+++ preventool/src/Application/Admin/SearchAdmin/SearchAdminHandler.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 namespace Preventool\Application\Admin\SearchAdmin;
 
 use Preventool\Domain\Admin\Model\Admin;
+use Preventool\Domain\Admin\Model\Value\AdminRole;
 use Preventool\Domain\Admin\Repository\AdminFilter;
 use Preventool\Domain\Admin\Repository\AdminRepository;
 use Preventool\Domain\Shared\Bus\Query\QueryHandler;
+use Preventool\Domain\Shared\Exception\ActionNotAllowedException;
+use Preventool\Domain\Shared\Model\Value\Uuid;
 use Preventool\Domain\Shared\Repository\QueryCondition\QueryCondition;
 
 class SearchAdminHandler implements QueryHandler
@@ -23,6 +26,15 @@ class SearchAdminHandler implements QueryHandler
         SearchAdminQuery $query
     ): SearchAdminResponse
     {
+
+        $actionAdminId = new Uuid($query->actionAdminId);
+        $actionAdmin = $this->adminRepository->findById(
+            $actionAdminId
+        );
+
+        if ($actionAdmin->getRole()->value != AdminRole::ADMIN_ROLE_ROOT){
+            throw ActionNotAllowedException::fromApplicationUseCase($actionAdminId);
+        }
 
         $filter = new AdminFilter(
             $query->filterById,
