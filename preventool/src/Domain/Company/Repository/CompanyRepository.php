bcda530d7b3f7a0523ba9aacab3c9afbdad345d9
Rafael Escalera <escalerarodriguezr@gmail.com>
2023-02-03 16:44:27 +0100

    feature/13-search-company/complete

--- preventool/src/Domain/Company/Repository/CompanyRepository.php
+++ preventool/src/Domain/Company/Repository/CompanyRepository.php
@@ -6,12 +6,20 @@ namespace Preventool\Domain\Company\Repository;
 use Preventool\Domain\Company\Model\Company;
 use Preventool\Domain\Company\Model\Value\LegalDocument;
 use Preventool\Domain\Shared\Model\Value\Uuid;
+use Preventool\Domain\Shared\Repository\QueryCondition\QueryCondition;
+use Preventool\Domain\Shared\Repository\Response\PaginatedQueryResponse;
 
 interface CompanyRepository
 {
 
     public function save(Company $company): void;
     public function findById(Uuid $id): Company;
+
+    public function searchPaginated(
+        QueryCondition $queryCondition,
+        CompanyFilter $filter,
+        bool $fetchJoinCollections
+    ): PaginatedQueryResponse;
     public function findByLegalDocumentOrNull(LegalDocument $legalDocument): ?Company;
 
 }
--- preventool/src/Infrastructure/Persistence/Doctrine/Repository/Company/DoctrineCompanyRepository.php
+++ preventool/src/Infrastructure/Persistence/Doctrine/Repository/Company/DoctrineCompanyRepository.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 namespace Preventool\Infrastructure\Persistence\Doctrine\Repository\Company;
 
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Preventool\Domain\Company\Exception\CompanyAlreadyExistsException;
 use Preventool\Domain\Company\Exception\CompanyNotFoundException;
 use Preventool\Domain\Company\Model\Company;
 use Preventool\Domain\Company\Model\Value\LegalDocument;
+use Preventool\Domain\Company\Repository\CompanyFilter;
 use Preventool\Domain\Company\Repository\CompanyRepository;
 use Preventool\Domain\Shared\Model\Value\Uuid;
+use Preventool\Domain\Shared\Repository\QueryCondition\QueryCondition;
+use Preventool\Domain\Shared\Repository\Response\PaginatedQueryResponse;
 use Preventool\Infrastructure\Persistence\Doctrine\Repository\DoctrineBaseRepository;
 
 class DoctrineCompanyRepository extends DoctrineBaseRepository implements CompanyRepository
@@ -46,6 +51,56 @@ class DoctrineCompanyRepository extends DoctrineBaseRepository implements Compan
         }
 
         return $company;
+    }
+
+    public function searchPaginated(
+        QueryCondition $queryCondition,
+        CompanyFilter $filter,
+        bool $fetchJoinCollections=false
+    ): PaginatedQueryResponse
+    {
+        $queryBuilder = $this->search($filter);
+        $queryBuilder
+            ->setFirstResult($queryCondition->getPageSize() * ($queryCondition->getCurrentPage()-1))
+            ->setMaxResults($queryCondition->getPageSize())
+            ->orderBy(sprintf('c.%s',$queryCondition->getOrderBy()), $queryCondition->getOrderDirection());
+
+
+        $paginator = new Paginator($queryBuilder->getQuery(), $fetchJoinCollections);
+        $total = $paginator->count();
+        $pages = (int) ceil($total/$queryCondition->getPageSize());
+
+        return new PaginatedQueryResponse(
+            $total,
+            $pages,
+            $queryCondition->getCurrentPage(),
+            $paginator->getIterator()
+        );
+
+    }
+
+    private function search(CompanyFilter $filter): QueryBuilder
+    {
+        $queryBuilder = $this->objectRepository->createQueryBuilder('c');
+
+        if(!empty($filter->getFilterByName())){
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->like('c.name', ':name'))
+                ->setParameter(
+                    ':name',
+                    '%'.$filter->getFilterByName().'%'
+                );
+        }
+
+        if(!empty($filter->getFilterById())){
+            $queryBuilder->andWhere('c.id = :id')
+                ->setParameter(
+                    ':id',
+                    $filter->getFilterById()
+                );
+        }
+
+        return $queryBuilder;
     }
 
 }
