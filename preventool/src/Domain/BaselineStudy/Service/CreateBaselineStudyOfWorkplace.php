3fb3e97ff3c5d66cc2e4e41cd76ef775717784c8
Rafael Escalera <escalerarodriguezr@gmail.com>
2023-03-09 14:14:41 +0100

    fature/30-create-baseline-study-on-workplace-created/complete

---
+++ preventool/src/Domain/BaselineStudy/Service/CreateBaselineStudyOfWorkplace.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types=1);
+
+namespace Preventool\Domain\BaselineStudy\Service;
+
+
+use Preventool\Domain\BaselineStudy\Model\BaselineStudy;
+use Preventool\Domain\BaselineStudy\Repository\BaselineStudyRepository;
+use Preventool\Domain\Shared\Model\IdentityGenerator;
+use Preventool\Domain\Shared\Model\Value\CompliancePercentage;
+use Preventool\Domain\Shared\Model\Value\Uuid;
+use Preventool\Domain\Workplace\Model\Workplace;
+
+class CreateBaselineStudyOfWorkplace
+{
+
+
+    public function __construct(
+        private readonly GetBaselineStudyIndicatorsService $indicatorsService,
+        private readonly BaselineStudyRepository $baselineStudyRepository,
+        private readonly IdentityGenerator $identityGenerator
+    )
+    {
+    }
+
+    public function __invoke(
+        Workplace $workplace
+    ): void
+    {
+
+        $categoryIndicators = $this->indicatorsService->__invoke();
+
+        foreach ($categoryIndicators as $categoryIndicator){
+            $indicators = $categoryIndicator['indicators'];
+            foreach ($indicators as $indicator){
+                $this->processIndicator(
+                    $indicator['id'],
+                    $indicator['category'],
+                    $workplace
+                );
+            }
+        }
+    }
+
+    private function processIndicator(
+        string $indicator,
+        string $category,
+        Workplace $workplace
+    ):void
+    {
+
+        $uuid = new Uuid($this->identityGenerator->generateId());
+
+        $baselineIndicator = new BaselineStudy(
+            $uuid,
+            $workplace,
+            $category,
+            $indicator,
+            new CompliancePercentage(0)
+        );
+
+        $baselineIndicator->setCreatorAdmin(
+            $workplace->getCreatorAdmin()
+        );
+
+        $this->baselineStudyRepository->save($baselineIndicator);
+
+    }
+
+}
