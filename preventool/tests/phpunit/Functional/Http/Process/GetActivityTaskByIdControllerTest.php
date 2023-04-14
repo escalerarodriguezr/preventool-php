b4504eac31f2a0dddc06c4efdeaf658129f346d2
Rafael Escalera <escalerarodriguezr@gmail.com>
2023-04-14 11:55:16 +0200

    feature/40-activity-tasks/process-activity-task-fixtures

---
+++ preventool/tests/phpunit/Functional/Http/Process/GetActivityTaskByIdControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPUnit\Tests\Functional\Http\Process;
+
+use PHPUnit\Tests\Functional\Http\FunctionalHttpTestBase;
+use Preventool\Infrastructure\Persistence\Doctrine\DataFixtures\AdminFixtures;
+use Preventool\Infrastructure\Persistence\Doctrine\DataFixtures\CompanyFixtures;
+use Preventool\Infrastructure\Persistence\Doctrine\DataFixtures\ProcessActivityFixtures;
+use Preventool\Infrastructure\Persistence\Doctrine\DataFixtures\ProcessActivityTaskFixtures;
+use Preventool\Infrastructure\Persistence\Doctrine\DataFixtures\ProcessFixtures;
+use Preventool\Infrastructure\Persistence\Doctrine\DataFixtures\UserFixtures;
+use Preventool\Infrastructure\Persistence\Doctrine\DataFixtures\WorkplaceFixtures;
+
+class GetActivityTaskByIdControllerTest extends FunctionalHttpTestBase
+{
+    const END_POINT = 'api/v1/activity-task/%s';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    private function prepareDatabase(): void
+    {
+        $this->databaseTool->loadFixtures([
+            UserFixtures::class,
+            AdminFixtures::class,
+            CompanyFixtures::class,
+            WorkplaceFixtures::class,
+            ProcessFixtures::class,
+            ProcessActivityFixtures::class,
+            ProcessActivityTaskFixtures::class
+        ]);
+    }
+
+    public function testGetActivityTaskByIdSuccessResponse():void
+    {
+        $this->prepareDatabase();
+        $this->authenticatedRootClient();
+        self::assertSame(1,1);
+    }
+
+
+}
